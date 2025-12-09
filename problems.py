ea78c7f89c92840d085e1f2023fd1e126a27fa60
TuffMango <therealgeorege@gmail.com>
2025-12-09 17:16:07 +0000

    ig bro

---
+++ problems.py
@@ -0,0 +1,40 @@
+#challange 1
+Value = int(input("How many hours did you work today? "))
+if Value < 10 and Value>1:
+    print("you slept", 24-Value, ".")
+else:
+    print("error")
+
+#challange 2
+player = 1
+nums=[]
+ties = 0
+score = int(input("enter your score"))
+
+#idk
+
+
+
+
+    #challenge 3
+    #idk what it is asking the wording is too compliacted
+
+#challenge 4
+things = int(input("how many things do you have?" ))
+list=[]
+if things
+#idk how to do half of these
+
+
+
+
+
+
+
+
+
+
+
+
+
+
